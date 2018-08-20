64bea46b3cb86d7f08e6bf84aaa141e5c5ef7436
Dan Mace and Michael Donovan <michael@>
2018-08-20 16:58:24 -0700

    so much timeout

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 
 var taskCards = [];
+var activeCards = [];
+var overdueCards = [];
 var fullDayNames = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
 var fullMonthNames = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 $(function(){
@@ -11,41 +13,98 @@ $(function(){
     var newTask = new Task(userTask,userDueTime, priorityOption)
   });
 });
-var timeUpdate = setInterval(function(){
-  taskCards.forEach(function(card,i){
-    var timeLeftMS = card.timeLeft()
-    $('#due-date-'+i).text(timeLeftMS)
+var timeUpdateLoop = setInterval(function(){
+  activeCards.forEach(function(card,i){
+    var timeLeft = card.timeLeft()
+    var timeLeftMS = parseInt(card.dueTime - Date.now())
+    console.log("this.dueTime " + card.dueTime)
+    console.log("Date.now() " + Date.now())
+    console.log(timeLeftMS)
+    if (timeLeftMS <= 0 && !card.throbbing) {
+      $('#task-card-'+card.index+'>.card-body').css({
+        'background-color' : 'green'
+      })
+      console.log("turning card " + i + " green!")
+      var amountOverdue = Math.abs(timeLeftMS)
+      console.log("overdue by " + amountOverdue)
+      $('#due-date-'+card.index).text("OVERDUE by " + msToTime(amountOverdue))
+      // activeCards.splice(i,1)
+      overdueCards.push(card)
+      card.throbbing = setInterval(function(){
+        card.throb(1.01);
+      },300)
+    } else {
+      $('#due-date-'+i).text(timeLeft)
+    }
   })
 },1000)
+
+// var throbLoop = setInterval(function(){
+  // overdueCards.forEach(function(card,i){
+  //   if (!card.throbbing) {
+  //     if (i % 2 === 0) {
+  //       card.throbbing = true
+  //       setTimeout(function(){
+  //         card.throb(1.01);
+  //       },300)
+  //     } else {
+  //       card.throb(1.01);
+  //     }
+  //   }
+  // })
+// },2000)
+
 function Task(description,dueTime,priority) {
   this.description = description;
   this.dueTime = dueTime;
   this.priority = priority;
   this.div = undefined
+  this.index = taskCards.length;
+  this.throbbing = false;
   taskCards.push(this)
+  activeCards.push(this)
   this.createCard()
 }
 Task.prototype.timeLeft = function() {
   return msToTime(this.dueTime - Date.now())
 }
+Task.prototype.throb = function(amount) {
+  var element = `#task-card-`+this.index
+  $(element).css({
+    'transform' : 'scaleX('+(amount*1.01)+') scaleY('+(amount*1.01)+')'
+  });
+  setTimeout(function(){
+    $(element).css({
+      'transform' : 'scaleX(1) scaleY(1)'
+    });
+    setTimeout(function(){
+      $(element).css({
+        'transform' : 'scaleX('+(amount)+') scaleY('+(amount)+')'
+      });
+      setTimeout(function(){
+        $(element).css({
+          'transform' : 'scaleX(1) scaleY(1)'
+        });
+      },100);
+    },100);
+  },100);
+}
 Task.prototype.createCard = function() {
-  var index = taskCards.length-1;
+  var index = this.index
   var checkbox = `<button id="done-button-`+index+`" type="button" class="btn btn-success done-button">Done!</button>`;
-
   $('#display-area').append(
     `<div id="task-card-`+index+`" class="card task-card">
       <div class="card-body ` + this.priority + `Pri"><div class="row"><div class="col">` + this.description + `</div><div class="col">` + prettyDate(this.dueTime) + `</div><div class="col"><div id="due-date-`+index+`">`+this.timeLeft()+`</div></div><div class="col">` + checkbox + `</div></div></div>`
     );
   $('#done-button-'+index).click(function () {
-
     $(`#task-card-`+index).animate({
       'height': 0,
       'opacity': 0
     },500)
-  setTimeout(function() {
-    $('#task-card-'+index).remove();
-  },500);
-  this.div = $(`#task-card-`+index)
+    setTimeout(function() {
+      $('#task-card-'+index).remove();
+    },500);
+    this.div = $(`#task-card-`+index)
   });
 }
 function msToTime(duration) {
